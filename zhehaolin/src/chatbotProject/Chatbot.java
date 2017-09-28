43cb519365f3b1eb78536e940231a851f657cc73
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-09-28 11:45:07 -0400

    b

--- zhehaolin/src/chatbotProject/Chatbot.java
+++ zhehaolin/src/chatbotProject/Chatbot.java
@@ -1,5 +1,33 @@
 package chatbotProject;
 
 public class Chatbot {
+	
+	private String userName;
+	private Topic zhehao;
+	private boolean chatting;
+	
+	
+	public Chatbot()
+	{
+		zhehao = new ChatbotZhe();
+		userName = "unknown user";
+		chatting = true;
+	}
+
+	public void startTalking() {
+		ChatbotMain.print("Welcome to our chatbox! what is your name?");
+		userName = ChatbotMain.getInput();
+		while(chatting) {
+			ChatbotMain.print("what do you want to talk about?");
+			String response = ChatbotMain.getInput();
+			if(zhehao.isTriggered(response)) {
+				chatting = false;
+				zhehao.startChatting();
+			}else {
+				ChatbotMain.print("I am sorry. I don't understand");
+			}
+		}
+		
+	}
 
 }
