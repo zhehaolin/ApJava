82cdfbc3e36450fe4f91ae6758c5e7fb9666fbd4
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-10-17 11:47:36 -0400

    hi

--- zhehaolin/src/arrays/ArraysMain.java
+++ zhehaolin/src/arrays/ArraysMain.java
@@ -6,17 +6,33 @@ public class ArraysMain {
 	
 	private String[] testArray;
 	private int[] intRay;
+	private int rolls;
 	
 	
 	public ArraysMain() {
 		intRay = new int[100];
+		int[] randomrolls = new int[1000];
+		populate(randomrolls);
+		
+		int[] result=longestconsecsqeandpos(randomrolls);
+		System.out.println("The longest sequence of dice rolls is " + result[0] + " it happened on the " + (result[1]+1)+"th roll. Starting with a roll of " +randomrolls[result[1]]+".");
+		
 		//populate(intRay);
 		//checkOccurences(intRay,3,18);
-		populate1toN(intRay);
-		swap(intRay,0,1);
-		shuffle(intRay);
+		//populate1toN(intRay);
+		//swap(intRay,0,1);
+		//reverseArray(intRay);
+		//fronttoback(intRay);
 		//arrays is a utility calls included in java for formatting
-		System.out.println(Arrays.toString(intRay));
+		//System.out.println(Arrays.toString(intRay));
+		
+	}
+	public int[] longestconsecsqeandpos(int[] arr)
+	{
+		int[] data = new int[2];
+		data[0]=longestconsecutivesequence(arr);
+		data[1]=rolls;
+		return data;
 	}
 	private void shuffle(int[] arr) {
 		for(int i=0; i<arr.length;i++)
@@ -24,6 +40,84 @@ public class ArraysMain {
 			swap(arr,(int) (Math.random()*arr.length),(int) (Math.random()*arr.length));
 		}
 		
+	}
+	private int[] reverseArray(int[] arr)
+	{
+		int[] reverseArray = new int[arr.length];
+		for(int i=0;i<arr.length;i++)
+		{
+			reverseArray[i]=arr[arr.length-(i+1)];
+		}
+		return reverseArray;
+		
+	}
+	public void fronttoback(int[] arr)
+	{
+		int temp=arr[0];
+		for(int i=0;i<arr.length;i++)
+		{
+			if(i==0)
+			{
+				arr[arr.length-1]=temp;
+			}else
+			{
+				arr[i-1]=arr[i];
+			}
+			
+		}
+		
+	}
+	public void cycleThrough(int[] arr, int n)
+	{
+		for(int i=0;i<n;i++)
+		{
+			fronttoback(arr);
+		}
+	}
+	//return the length of the longest sequence of consecutive intergers 
+	//for example lcs({1,2,3,7,8,9,10,})
+	public int longestconsecutivesequence(int[] arr)
+	{
+		int count=1;
+		int currentmax=1;
+		for(int i=0;i<arr.length-1;i++)
+		{
+			if(isconsecutive(arr,i))
+			{
+				count+=1;
+				
+			}else {
+				if(count>currentmax)
+				{
+					currentmax=count;
+					rolls=i-currentmax;
+					count=1;
+				}
+				count=1;
+				
+			}
+			
+			
+		}
+		return currentmax;
+	}
+	
+	public boolean isconsecutive(int[] arr,int n)
+	{
+		if(arr[n]+1==arr[n+1])
+		{
+			return true;
+		}
+		return false;
+	}
+	public int countlessthan(int[] arr,int n)
+	{
+		int count=0;
+		for(int value: arr)
+		{
+			if(value<n)count++;
+		}
+		return count;
 	}
 	private void swap(int[] arr, int s, int e) {
 		int temp=arr[s];
@@ -54,7 +148,7 @@ public class ArraysMain {
 		
 		for(int i=0; i<intRay.length;i++)
 		{
-			intRay[i] = diceRoll(3);
+			intRay[i] = diceRoll(2);
 			
 		}
 	}
