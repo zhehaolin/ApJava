555911c023164aa9abb816295f0193e3be7da7c7
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-10-31 11:43:21 -0400

    gg

---
+++ zhehaolin/src/Advanture/NPC.java
@@ -0,0 +1,24 @@
+package Advanture;
+
+public class NPC {
+
+	public NPC() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean isActive() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void interact() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getInactiveDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ zhehaolin/src/Advanture/NPCRoom.java
@@ -0,0 +1,78 @@
+package Advanture;
+
+public class NPCRoom extends CaveRoom {
+	private NPC presentNPC;
+	
+	
+	public NPCRoom(String description) {
+		super(description);
+		presentNPC= null;
+	}
+	/*
+	 * NPCs can enter a room if no other npc is in that room
+	 * 
+	 */
+	public boolean CanEnter() {
+		return presentNPC == null;
+	}
+	public void enterNPC(NPC m)
+	{
+		presentNPC = m;
+	}
+	public void leaveNPC()
+	{
+		presentNPC= null;
+	}
+	public boolean containsNPC()
+	{
+		return presentNPC !=null;
+	}
+	
+	//the above methods are new features to a caveroom,
+	//the methods below replace caveroom methods(override)
+	
+	
+	public String validKeys()
+	{
+		return "wdsae";
+	}
+	public void printAllowedEntry()
+	{
+		System.out.print("Only enter w a s d to move or" +" you can type e to interact. ");
+	}
+	public void performAction(int direction) {
+		if(direction ==4) {
+			if(containsNPC() && presentNPC.isActive()) {
+				presentNPC.interact();
+			}else {
+				CaveExplorer.print("there is nothing to interact with.");
+			}
+		}else
+		{
+			CaveExplorer.print("That key does nothing.");
+		}
+	}
+	public String getContents()
+	{
+		if(containsNPC() && presentNPC.isActive())
+		{
+			return "M";
+		}else {
+			//return what would be return otherwise
+			return super.getContents();
+		}
+	}
+	public String getDescription()
+	{
+		if(containsNPC() && !presentNPC.isActive()) {
+			return super.getDescription() +"\n" + presentNPC.getInactiveDescription();
+		}else
+		{
+			return super.getDescription();
+		}
+	}
+	
+	
+	
+	
+}
