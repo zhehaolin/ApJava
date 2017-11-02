c9729dda025cb66c0ea840c33c7773b84e25d63c
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-11-02 11:15:56 -0400

    z

--- zhehaolin/src/Advanture/NPC.java
+++ zhehaolin/src/Advanture/NPC.java
@@ -67,8 +67,8 @@ public class NPC {
 	public void act() {
 		if(active) {
 			int[] move = calculateMovement();
-			int newRow = currentRow + move[0];
-			int newCol = currentCol + move[1];
+			int newRow = move[0];
+			int newCol = move[1];
 			setPosition(newRow,newCol);
 		}
 	}
