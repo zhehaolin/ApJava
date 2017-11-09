bf5209140b29ddf580e6f2509ced2544c7a6b4ce
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-11-09 10:43:20 -0500

    zhe

---
+++ zhehaolin/src/javeTheHardWay/Environment.java
@@ -0,0 +1,32 @@
+package javeTheHardWay;
+
+public class Environment {
+	
+	private int plants;
+	private int growthRate;
+	public static void main(String[] args) {
+		Environment farm = new Environment(7);
+		System.out.println(farm);
+		farm.growPlants();
+		
+	}
+	public Environment(int plants) {
+		this.plants = plants;
+		this.growthRate = plants/2;
+	}
+	public void growPlants() {
+		plants+=growthRate;
+		System.out.println("\n There are "+plants+" plants.");
+	}
+	public boolean eatPlants() {
+		if(plants >0) {
+			plants--;
+			return true;
+		}
+		return false;
+	}
+	public String toString() {
+		return " An vast field";
+	}
+
+}
