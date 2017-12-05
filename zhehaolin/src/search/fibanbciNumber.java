dd12c2a61a5a882cb591e0dd1b0f92703af5ec5c
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2017-12-05 09:44:53 -0500

    qwe

---
+++ zhehaolin/src/search/fibanbciNumber.java
@@ -0,0 +1,16 @@
+package search;
+
+public class fibanbciNumber {
+	private int count=0;
+	public static void main(String[] args) {
+		
+		FNumber(3);
+	}
+	public static int FNumber(int n) {
+		if(n==1) {
+			return 1;
+		}else {
+			
+		}
+	}
+}
